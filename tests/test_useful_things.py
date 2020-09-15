485f8e69e8753be7eaad164fa4cf71a1c22969ee
shahramn <shahram.najm@ecmwf.int>
2020-09-15 11:55:20 +0100

    Update test_useful_things.py

--- tests/test_useful_things.py
+++ tests/test_useful_things.py
@@ -5,9 +5,11 @@ import hypothesis.strategies as st
 
 @given(st.integers(), st.integers())
 def test_add(x, y):
+    print(f'add {x} to {y}')
     assert useful_things.add(x, y) == x + y
 
 
 @given(st.integers(), st.integers())
 def test_subtract(x, y):
+    print('sub ', x, y)
     assert useful_things.subtract(x, y) == x - y
